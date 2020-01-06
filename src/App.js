9789c04e1c45a1fe076c3dba061a6b5f6efd74e5
Dailan Bueno <daylansantos@gmail.com>
2020-01-05 21:05:57 -0300

    Iniciando com hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tech, setTech] = useState(['ReactJs', 'React Native']);
+    const [inputText, setInputText] = useState('');
+    const handleAdd = () => {
+        setTech([...tech, inputText]);
+        setInputText('');
+    };
+
+    const handleChange = e => {
+        setInputText(e.target.value);
+    };
+
+    return (
+        <>
+            <ul>
+                {tech.map(item => (
+                    <li>{item}</li>
+                ))}
+            </ul>
+            <input type="text" onChange={handleChange} value={inputText} />
+            <button type="button" onClick={handleAdd}>
+                Add
+            </button>
+        </>
+    );
 }
 
 export default App;
